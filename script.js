a88f3d035bf26150c0ccd3f910074c1867a772a2
Dimitris Efstathiou <jimefsta2@gmail.com>
2023-12-07 23:54:22 +0200

    u

--- script.js
+++ script.js
@@ -5,53 +5,54 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenuToggle.addEventListener('click', function () {
         nav.classList.toggle('active');
     });
-});
 
-
-
-const quoteForm = document.querySelector('#quoteForm');
-const submitBtn = document.querySelector('.submit-btn');
-const nameInput = document.querySelector('#fullName');
-const emailInput = document.querySelector('#email');
-const sqmInput = document.querySelector('#squareMeters');
-const surfaceTypeInput = document.querySelector('#surfaceType');
-const locInput = document.querySelector('#location');
-const addInfoInput = document.querySelector('#additionalInfo');
-
-
-
-const publicKey = "B45szz-pbyYRbZ2c6";
-const serviceId = "service_geki7gp";
-const templateId = "template_f2rgmn5";
-
-
-// Initialize Email.js with your user ID
-emailjs.init(publicKey);
-
-quoteForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    submitBtn.innerText = "Μισό δευτερόλεπτο...";
-
-    const inputFields = {
-        name: nameInput.value,
-        email: emailInput.value,
-        squareMeters: sqmInput.value,
-        surfaceType: surfaceTypeInput.value,
-        location: locInput.value,
-        additionalInfo: addInfoInput.value,
-    };
-
-    try {
-        await emailjs.send(serviceId, templateId, inputFields);
-        submitBtn.innerText = "Αποστολή";
-        nameInput.value = "";
-        emailInput.value = "";
-        sqmInput.value = "";
-        surfaceTypeInput.value = "";
-        locInput.value = "";
-        addInfoInput.value = "";
-    } catch (error) {
-        console.log(error);
-        submitBtn.innerText = "Κάτι πήγε στραβά";
+    // Separate function to initialize Email.js
+    function initEmailJs() {
+        const publicKey = "B45szz-pbyYRbZ2c6";
+        emailjs.init(publicKey);
     }
+
+    // Initialize Email.js
+    initEmailJs();
+
+    const serviceId = "service_geki7gp";
+    const templateId = "template_f2rgmn5";  
+
+    const quoteForm = document.querySelector('#quoteForm');
+    const submitBtn = document.querySelector('.submit-btn');
+    const nameInput = document.querySelector('#fullName');
+    const emailInput = document.querySelector('#email');
+    const sqmInput = document.querySelector('#squareMeters');
+    const surfaceTypeInput = document.querySelector('#surfaceType');
+    const locInput = document.querySelector('#location');
+    const addInfoInput = document.querySelector('#additionalInfo');
+
+    quoteForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        submitBtn.innerText = "Μισό δευτερόλεπτο...";
+
+        const inputFields = {
+            name: nameInput.value,
+            email: emailInput.value,
+            squareMeters: sqmInput.value,
+            surfaceType: surfaceTypeInput.value,
+            location: locInput.value,
+            additionalInfo: addInfoInput.value,
+        };
+
+        try {
+            await emailjs.send(serviceId, templateId, inputFields);
+            submitBtn.innerText = "Αποστολή";
+            nameInput.value = "";
+            emailInput.value = "";
+            sqmInput.value = "";
+            surfaceTypeInput.value = "";
+            locInput.value = "";
+            addInfoInput.value = "";
+        } catch (error) {
+            console.log(error);
+            submitBtn.innerText = "Κάτι πήγε στραβά";
+            // Display error message to the user if needed
+        }
+    });
 });
